96d31f155603656b42ddd5586757f055f6db6b45
maksuka <maksukay@mail.ru>
2020-12-29 21:13:08 +0300

    Integer

--- lessons/Program.cs
+++ lessons/Program.cs
@@ -10,7 +10,16 @@ namespace lessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("cheeel");
+            byte bytik = 1; //1 байт от 0 до 255
+            byte sbytik = 1; //1 байт от -128 до 127
+            byte shortik = 1; //2 байт от -32768 до 32767
+            byte ushortik = 1; //2 байт от 0 до 65535
+            byte intik = 1; //4 байт от -2млрд до 2млрд
+            byte uintik = 1; //4 байт от 0 до 4млрд
+            byte longik = 1; //8 байт от -дохуя до дохуя
+            byte ulongik = 1; //8 байт от 0 до хуя
+
+            Console.WriteLine(bytik + sbytik + shortik + ushortik + intik + uintik + longik + ulongik);
         }
     }
 }
