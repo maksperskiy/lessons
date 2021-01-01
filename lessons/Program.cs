8dbc606e7a9c17c62b4dc1bc948256676dad4186
maksuka <maksukay@mail.ru>
2021-01-01 20:53:42 +0300

    Convertation

--- lessons/Program.cs
+++ lessons/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace lessons
 {
@@ -10,17 +11,22 @@ namespace lessons
     {
         static void Main(string[] args)
         {
-            byte bytik = 1; //1 байт от 0 до 255
-            byte sbytik = 1; //1 байт от -128 до 127
-            byte shortik = 1; //2 байт от -32768 до 32767
-            byte ushortik = 1; //2 байт от 0 до 65535
-            byte intik = 1; //4 байт от -2млрд до 2млрд
-            byte uintik = 1; //4 байт от 0 до 4млрд
-            byte longik = 1; //8 байт от -дохуя до дохуя
-            byte ulongik = 1; //8 байт от 0 до хуя
+            //Convertation WAOW!
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",//Цудоуна
+            };
+            string str = "1", str2 = "2";
+
+            int a = Convert.ToInt32(str);
+            int b = Convert.ToInt32(str2);
+            Console.WriteLine(a + b);
+
+            str = "1.5";
+
+            Console.WriteLine(Convert.ToDouble(str, numberFormatInfo));
 
 
-            Console.WriteLine(Console.ReadLine()/*так можно lulw*/ + bytik + sbytik + shortik + ushortik + intik + uintik + longik + ulongik);
         }
     }
 }
